dc2ae95999e69c71e64faf0ef1cf4c9af0ebfa27
AnderPeroza <109151281+AnderPeroza@users.noreply.github.com>
2022-09-07 11:58:34 -0400

    Add files via upload

--- index-info-movies.js
+++ index-info-movies.js
@@ -34,5 +34,80 @@ const init =async ()=>{
         
     `
     infoMovie.innerHTML = data.data.overview
+    const btnComprar = document.getElementById("btn-comprar")
+    if(!localStorage.getItem('token')){
+        btnComprar.style.display = "none"
+    }
+
+    
 }
 init()
+
+const btnComprar = document.getElementById("btn-comprar")
+
+btnComprar.addEventListener("click", async ()=>{
+
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+    const response = await fetch(`https://vg-cine-server.herokuapp.com/movie-detail/${params.id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+    const data = await response.json()
+    const { value: formValues } = await Swal.fire({
+        title: 'Multiple inputs',
+        html:`
+            <label for="">Cantidad Entradas</label>
+            <input id="ticket-cant" class="swal2-input"></input>
+            
+            <label for="">Metodo de Pago</label>
+            <input id="pago" class="swal2-input"></input>
+            <br>
+            <label for="">Cedula</label>
+            <br>
+            <input id="cedula" class="swal2-input"> </input>
+            
+            <label for="">Numero de telef</label>
+            <input id="numero-ref" class="swal2-input"> </input>
+        
+        `,
+          
+        focusConfirm: false,
+        preConfirm:  async () => {
+            return {
+                ticketCount: document.getElementById('ticket-cant').value,
+                paymentMethod : document.getElementById('pago').value,
+                id : document.getElementById('cedula').value,
+                referenceNumber : document.getElementById('numero-ref').value,
+                movieTitle : data.data.title
+
+            }
+        //   return [
+        //     document.getElementById('swal-input1').value,
+        //     document.getElementById('swal-input2').value
+        //   ]
+        }
+    })
+      
+    if (formValues) {
+        const token =  localStorage.getItem('token')
+        const params = Object.fromEntries(urlSearchParams.entries());
+        const response = await fetch(`https://vg-cine-server.herokuapp.com/ticket`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': `Bearer ${token}`
+            },
+            body:  JSON.stringify(formValues)
+        })
+        if(response.status === 200){
+            console.log("listo Ppi")
+        }
+    }
+
+
+
+})
--- index.js
+++ index.js
@@ -12,35 +12,68 @@ const init= async()=>{
             <a class="nav-link active" aria-current="page" href="index-perfil.html">Perfil</a>
         `
     }
-
-    const response = await fetch('https://vg-cine-server.herokuapp.com/movies', {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-
-    const data = await response.json()
-    const movies = document.getElementById("movies")
-    
-    
-    for(let i = 0 ; i<data.data.length; i++){
+    if(!window.location.href.includes("index-all-movies.html")){
+            const response = await fetch('https://vg-cine-server.herokuapp.com/movies', {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const data = await response.json()
+        const movies = document.getElementById("movies")
+        console.log(data)
         
-        movies.innerHTML+=`
-            <div data-aos="fade-right"
-            data-aos-offset="300"
-            data-aos-easing="ease-in-sine" >
-                <a href="index-info-movies.html?id=${data.data[i].id}"><img  src="https://image.tmdb.org/t/p/w500${data.data[i].poster_path}" alt=""></a>
-            </div>
-           
-         `
-    }
+        for(let i = 0 ; i<data.data.length; i++){
+            
+            movies.innerHTML+=`
+                <div data-aos="fade-right"
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine" >
+                    <a href="index-info-movies.html?id=${data.data[i].id}"><img  src="https://image.tmdb.org/t/p/w500${data.data[i].poster_path}" alt=""></a>
+                </div>
+            
+            `
+        }
 
-    const banner = document.getElementById("banner")
-    banner.style.backgroundImage = 'url("https://cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/A52OT4DFRNCGRDDGJ2LGL7R4J4.jpg")'
-    banner.style.height = "85vh"
-    banner.style.backgroundPosition = "center"
-    banner.style.backgroundSize = "cover"
+        const banner = document.getElementById("banner")
+        banner.style.backgroundImage = 'url("https://cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/A52OT4DFRNCGRDDGJ2LGL7R4J4.jpg")'
+        banner.style.height = "85vh"
+        banner.style.backgroundPosition = "center"
+        banner.style.backgroundSize = "cover"
+    }else{
+        const response = await fetch('https://vg-cine-server.herokuapp.com/movies?all=true', {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const data = await response.json()
+        const movies = document.getElementById("movies")
+        console.log(data)
+        
+        for(let i = 0 ; i<data.data.length; i++){
+            
+            movies.innerHTML+=`
+                <div data-aos="fade-right"
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine" >
+                    <a href="index-info-movies.html?id=${data.data[i].id}"><img  src="https://image.tmdb.org/t/p/w500${data.data[i].poster_path}" alt=""></a>
+                </div>
+            
+            `
+        }
+
+        const banner = document.getElementById("banner")
+        banner.style.backgroundImage = 'url("https://cloudfront-us-east-1.images.arcpublishing.com/gruporepublica/A52OT4DFRNCGRDDGJ2LGL7R4J4.jpg")'
+        banner.style.height = "85vh"
+        banner.style.backgroundPosition = "center"
+        banner.style.backgroundSize = "cover"
+         
+    }   
+    console.log(location.href)
+
+   
+    
     
 
     
